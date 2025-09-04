a241fd46fc3a8373627fba8c296d41e4ecd6c132
Hasan Hasan <Hjhasan@umich.edu>
2025-09-04 13:16:35 -0400

    Add ClassmateInfo program and screenshot

---
+++ src/Classmateinfoo.java
@@ -0,0 +1,9 @@
+public class ClassmateInfo {
+    public static void main(String[] args) {
+        System.out.println("Name: Mohammed / He, Him");
+        System.out.println("Major: Software engineer ");
+        System.out.println("Fun Fact: I love playing and watching soccer!");
+    }
+}
+
+
