34a811243fbce6ca7314ada6d94b3311f3fa121d
Hasan Hasan <Hjhasan@umich.edu>
2025-09-04 17:24:45 -0400

    Update ClassmateInfo program and sync with GitHub

--- src/ClassmateInfo.java
+++ src/ClassmateInfo.java
@@ -2,7 +2,9 @@ public class ClassmateInfo {
     public static void main(String[] args) {
         System.out.println("Name: Mohammed / He, Him");
         System.out.println("Major: Software engineer ");
+        System.out.println("Some CIS classes including 3501");
         System.out.println("Fun Fact: I love playing and watching soccer!");
+        System.out.println("System.out.println(\"Fun Fact: I love playing and watching soccer!\");");
     }
 }
 
